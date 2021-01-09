da1c5364e972b6da8b3d883cdff75fb6a22b8af2
whatjackhasmade <jack@noface.co.uk>
2021-01-09 15:31:17 +0000

    Handle inactive status

--- src/controllers/status/getStatusFromRepo.ts
+++ src/controllers/status/getStatusFromRepo.ts
@@ -7,7 +7,9 @@ import type { Request, Response } from "express";
 // Common
 import { logger } from "vercel-status";
 
-const getStatus = async ({ headers, url }): Promise<"pending" | "success"> => {
+declare type DeployStatus = "inactive" | "pending" | "success";
+
+const getStatus = async ({ headers, url }): Promise<DeployStatus> => {
   const res = await fetch(url, { headers });
   const data = await res.json();
   const latest = data?.[0];
@@ -16,30 +18,41 @@ const getStatus = async ({ headers, url }): Promise<"pending" | "success"> => {
 };
 
 const getStatusFromRepo = async (req: Request, res: Response): Promise<void> => {
-  const username: string = req.params.username;
   const repo: string = req.params.repo;
+  const username: string = req.params.username;
 
+  // This should be set in your .ENV file
   const GITHUB_TOKEN = process.env.GITHUB_TOKEN || "";
-  const headers = {
-    Authorization: `token ${GITHUB_TOKEN}`,
-  };
 
-  const url = `https://api.github.com/repos/${username}/${repo}/deployments`;
+  // Using headers means we get an increase in the amount of requests we can make to Github
+  const headers = { Authorization: `token ${GITHUB_TOKEN}` };
+
+  // Build the repository deployments URL from the parameters in the URL
+  const repoDeployments = `https://api.github.com/repos/${username}/${repo}/deployments`;
 
   try {
-    const allDeploymentsRes = await fetch(url, {
-      headers,
-    });
-    const allDeploymentsData = await allDeploymentsRes.json();
-
-    const latestDeployment = allDeploymentsData?.[0];
-    const latestDeploymentStatusURL = latestDeployment.statuses_url;
-
-    const status = await getStatus({ headers, url: latestDeploymentStatusURL });
-
+    // Tell the browser we are returning an SVG file
     res.setHeader("Content-Type", "image/svg+xml");
 
+    // Get an array of all the deployments Vercel has attempted for this repository
+    const allDeploymentsRes = await fetch(repoDeployments, { headers });
+    const allDeploymentsData = await allDeploymentsRes.json();
+
+    // Get the latest deployment from the list of all deployments
+    const latestDeployment = allDeploymentsData?.[0];
+
+    // Get the URL with the detailed status object
+    const url: string = latestDeployment.statuses_url;
+
+    // Get the status of the latest deployment
+    const status: DeployStatus = await getStatus({ headers, url });
+
     switch (status) {
+      case "inactive": {
+        res.sendFile(path.join(__dirname + "../../../views/success.html"));
+        break;
+      }
+
       case "pending": {
         res.sendFile(path.join(__dirname + "../../../views/pending.html"));
         break;
